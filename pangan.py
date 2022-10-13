3c18b52aea7477835bd7d794f1fe2429df6d785f
Satrio Adi Prawiro <prawiro.96@gmail.com>
2022-10-13 10:04:56 +0700

    update path file in panga .py

--- pangan.py
+++ pangan.py
@@ -117,12 +117,12 @@ Berikut grafik untuk pergerakan harga setiap komoditas pangan (periode bulanan) 
     )
     
     if market == 'Tradisional':
-        table1 = '.\daerah\harga-pasar-tradisional-daerah.xls'
+        table1 = '.\projekpangan\daerah\harga-pasar-modern-daerah.xls'
         for pang in komoditas:
             if pang :
                 graphic_line_cmdty(table1, 'tradisional', pang)
     elif market == 'Modern':
-        table2 = '.\daerah\harga-pasar-tradisional-modern.xls'
+        table2 = '.\projekpangan\daerah\harga-pasar-tradisional-modern.xls'
         for ngan in komoditas:
             if ngan :
                 graphic_line_cmdty(table2, 'modern', ngan)
